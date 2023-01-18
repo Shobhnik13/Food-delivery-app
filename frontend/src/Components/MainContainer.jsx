880126c53b3f4b180955ab051008ef9c7ea11f1e
shobhnik13 <shobhnikw@gmail.com>
2023-01-19 00:25:10 +0530

    made changes in maincontainer section

--- frontend/src/Components/MainContainer.jsx
+++ frontend/src/Components/MainContainer.jsx
@@ -6,9 +6,9 @@ const MainContainer = () => {
        {/* mobile view  */}
        {/* grid grid-cols-1  */}
        {/* it will contain every col of the grid as one same sized div  */}
-       <div className="flex flex-col flex-1 py-2 justify-center items-start md:items-center">
+       <div className="flex flex-col flex-1 py-2 justify-center items-start md:items-center gap-6">
         {/* div 1 will contain the headline and bike image  */}
-          <div className='flex items-center gap-2 justify-center bg-orange-100 p-2 rounded-full'>
+          <div className='flex items-center gap-2 justify-center bg-orange-100 p-2 rounded-full '>
             {/* p  */}
           <p className='text-orange-500 font-semibold px-4 py-1'>Bike Delivery</p>
           {/* div for image  */}
@@ -16,15 +16,16 @@ const MainContainer = () => {
             <img src={Delivery} alt="" className='w-full h-full object-contain'/>
           </div>
           </div>
-          
-
+        <p className='text-[2.5rem] font-bold tracking-wide text-headingColor'>The Fastest Delivery in <span className='text-orange-600 text-[3rem]'>Your City</span></p> 
+        <p className='text-textColor text-center md:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laboriosam consequuntur beatae quasi laudantium quam molestiae ad fugiat, quo explicabo nulla expedita dolorem ab quidem autem inventore, repellat possimus unde!</p>
+        <button className='bg-gradient-to-br from-orange-400 to-orange-500 py-2 rounded-lg hover:shadow-lg w-full transition-all duration-100 ease-in-out'>Order Now</button>
        </div>
 
 
        {/* laptop view  */}
        {/* md:grid-cols-2  */}
        {/* it will contain every col of the grid as the 2 same sized div  */}
-       <div className="flex-1 py-2"></div>
+       <div className="flex-1 py-2 bg-blue-500"></div>
     
     </div>
   )
