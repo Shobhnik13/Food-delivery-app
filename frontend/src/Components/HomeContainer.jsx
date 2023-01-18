000079e36049d256d7b16c3f5c4d539f57f020c2
shobhnik13 <shobhnikw@gmail.com>
2023-01-19 03:03:36 +0530

    added homecontainer and set in maincontainer

---
+++ frontend/src/Components/HomeContainer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Delivery from '../assets/delivery.png'
+import HeroBg from '../assets/heroBg.png'
+
+const HomeContainer = () => {
+  return (
+    <section className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full h-screen' id='home'>
+         {/* left side view */}
+       <div className="flex flex-col flex-1 py-2 justify-center items-start  gap-6">
+        {/* div 1 will contain the headline and bike image  */}
+          <div className='flex items-center gap-2 justify-center bg-orange-100 p-2 rounded-full '>
+            {/* p  */}
+          <p className='text-orange-500 font-semibold px-4 py-1'>Bike Delivery</p>
+          {/* div for image  */}
+          <div className='w-8 h-8 rounded-full overflow-hidden drop-shadow-xl bg-white'>
+            <img src={Delivery} alt="" className='w-full h-full object-contain'/>
+          </div>
+          </div>
+          {/* div2  */}
+        <p className='text-[2.5rem] lg:text-[4.25rem] font-bold tracking-wide text-headingColor'>The Fastest Delivery in <span className='text-orange-600 text-[3rem] lg:text-[5rem]'>Your City</span></p> 
+        {/* div3  */}
+        <p className='text-textColor text-center md:text-left md:w-[80%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laboriosam consequuntur beatae quasi laudantium quam molestiae ad fugiat, quo explicabo nulla expedita dolorem ab quidem autem inventore, repellat possimus unde!</p>
+        {/* div 4 */}
+        <button className='bg-gradient-to-br from-orange-400 to-orange-500 py-2 rounded-lg hover:shadow-lg w-full md:w-auto px-4 transition-all duration-100 ease-in-out'>Order Now</button>
+       </div>
+
+       {/* right view  */}
+       <div className="flex-1 py-2 items-center relative ">
+        {/* div 1 for img  */}
+            <img src={HeroBg} alt="" className='w-full md:w-auto h-420 ml-auto lg:h-[650px]' />
+        {/* div 2  */}
+        <div className='absolute flex w-full h-full items-center justify-center'>
+
+        </div>
+        </div>
+    
+    </section>
+  )
+}
+
+export default HomeContainer
--- frontend/src/Components/MainContainer.jsx
+++ frontend/src/Components/MainContainer.jsx
@@ -1,32 +1,9 @@
 import React from 'react'
-import Delivery from '../assets/delivery.png'
+import HomeContainer from './HomeContainer'
 const MainContainer = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-2 '>
-       {/* mobile view  */}
-       {/* grid grid-cols-1  */}
-       {/* it will contain every col of the grid as one same sized div  */}
-       <div className="flex flex-col flex-1 py-2 justify-center items-start md:items-center gap-6">
-        {/* div 1 will contain the headline and bike image  */}
-          <div className='flex items-center gap-2 justify-center bg-orange-100 p-2 rounded-full '>
-            {/* p  */}
-          <p className='text-orange-500 font-semibold px-4 py-1'>Bike Delivery</p>
-          {/* div for image  */}
-          <div className='w-8 h-8 rounded-full overflow-hidden drop-shadow-xl bg-white'>
-            <img src={Delivery} alt="" className='w-full h-full object-contain'/>
-          </div>
-          </div>
-        <p className='text-[2.5rem] font-bold tracking-wide text-headingColor'>The Fastest Delivery in <span className='text-orange-600 text-[3rem]'>Your City</span></p> 
-        <p className='text-textColor text-center md:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laboriosam consequuntur beatae quasi laudantium quam molestiae ad fugiat, quo explicabo nulla expedita dolorem ab quidem autem inventore, repellat possimus unde!</p>
-        <button className='bg-gradient-to-br from-orange-400 to-orange-500 py-2 rounded-lg hover:shadow-lg w-full transition-all duration-100 ease-in-out'>Order Now</button>
-       </div>
-
-
-       {/* laptop view  */}
-       {/* md:grid-cols-2  */}
-       {/* it will contain every col of the grid as the 2 same sized div  */}
-       <div className="flex-1 py-2 bg-blue-500"></div>
-    
+    <div className='w-full h-auto items-center justify-center flex flex-col'>
+      <HomeContainer/>
     </div>
   )
 }
