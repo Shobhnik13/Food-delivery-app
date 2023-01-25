153807ec4e2d275710731b8f3eb4527302d07f20
Shobhnik13 <shobhnikw@gmail.com>
2023-01-26 05:03:54 +0530

    changes

--- frontend/src/Utils/FirebaseFunctions.jsx
+++ frontend/src/Utils/FirebaseFunctions.jsx
@@ -1,4 +1,4 @@
-// here we will just saving the new items/details that the user will enter 
+// here we will just saving the new items/details that the user will enter
 import { doc, setDoc } from 'firebase/firestore'
 import React from 'react'
 import { db } from '../Firebase'
