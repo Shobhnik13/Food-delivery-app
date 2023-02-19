0f15edc495bf796557a7ccc0208d4bb0c8006abc
Shobhnik13 <shobhnikw@gmail.com>
2023-02-19 21:02:37 +0530

    fixed scroll and completed the fruit x-scroll section (flag-true)

--- frontend/src/Components/MainContainer.jsx
+++ frontend/src/Components/MainContainer.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeContainer from './HomeContainer'
 import { motion } from 'framer-motion'
 import { MdChevronLeft,MdChevronRight } from 'react-icons/md'
 import RowContainer from './RowContainer'
 import { useStateValue } from '../Context/StateProvider'
 const MainContainer = () => {
+  const [scrollValue,setScrollValue]=useState(0)
+  useEffect(()=>{
+  },[scrollValue])
   const [{fooditems},dispatch]=useStateValue()
   return (
     <div className='w-full h-auto items-center justify-center flex flex-col'>
@@ -14,16 +17,16 @@ const MainContainer = () => {
         {/* main div-contain a paragraph heading and 2logos div  */}
         <div className='w-full flex items-center justify-between'>
           {/* p    -div1  */}
-          <p className="before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 text-2xl font-semibold capitalize text-headingColor relative ">
+          <p className="before:absolute mt-4 before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 text-2xl font-semibold capitalize text-headingColor relative ">
             Our Fresh & Healthy Fruits!!!
           </p>
           {/* 2logo div  -div2 */}
           {/* itt will be hidden on mobile screes */}
           <div className=" items-center hidden gap-3 md:flex">
             {/* logo1 */}
-            <motion.div whileTap={{scale:0.75}} className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 flex items-center justify-center cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg"><MdChevronLeft className='text-lg'/></motion.div>
+            <motion.div onClick={()=>setScrollValue(-950)} whileTap={{scale:0.75}} className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 flex items-center justify-center cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg"><MdChevronLeft className='text-lg'/></motion.div>
             {/* logo2 */}
-            <motion.div whileTap={{scale:0.75}} className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 flex items-center justify-center cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg"><MdChevronRight  className='text-lg'/></motion.div>
+            <motion.div onClick={()=>setScrollValue(950)} whileTap={{scale:0.75}} className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 flex items-center justify-center cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg"><MdChevronRight  className='text-lg'/></motion.div>
           </div>
         </div>
         {/* bringing the row container in fruits section but after the para and 2 logo div (which will be only visible on web) */}
@@ -31,7 +34,8 @@ const MainContainer = () => {
         
         <RowContainer 
         flag={true}
-        data={fooditems}
+        scrollValue={scrollValue}
+        data={fooditems?.filter((item)=>item.category==='fruits')}
           />
       </section>
     </div>
--- frontend/src/Components/RowContainer.jsx
+++ frontend/src/Components/RowContainer.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { motion } from 'framer-motion'
 import dummyimage from '../assets/c2.png'
 import { MdShoppingBasket } from 'react-icons/md'
 import { useStateValue } from '../Context/StateProvider'
-const RowContainer = ({flag,data}) => {
+const RowContainer = ({flag,data,scrollValue}) => {
   // console.log(data)
+  const rowScroll=useRef()
+  useEffect(()=>{
+    rowScroll.current.scrollLeft +=scrollValue
+  },[scrollValue])
   return (
     
     // main div-conatin multiple combined image and cart div combinations->card comp divs 
-    <div className={`w-full flex items-center my-12 gap-3
+    <div ref={rowScroll} className={`w-full scroll-smooth flex items-center my-12 gap-3
      ${
       flag? `overflow-x-scroll scrollbar-none ` : `overflow-x-hidden flex-wrap`
       }
@@ -16,22 +20,22 @@ const RowContainer = ({flag,data}) => {
       >
         {
           data && data.map(item=>(
-            <div key={item.id} className='w-300 min-w-[300px] md:min-w-[340px] md:w-340 hover:drop-shadow-lg shadow-md backdrop-blur-lg h-auto my-6 bg-cardOverlay rounded-lg p-2'> 
+            <div key={item.id} className='w-300 min-w-[300px] md:min-w-[340px] md:w-340 hover:drop-shadow-lg shadow-md backdrop-blur-lg h-[200px] my-6 bg-cardOverlay rounded-lg p-2'> 
         <div className='w-full flex items-center justify-between'>
           <motion.img
           whileHover={{scale:1.2}}
-          src={dummyimage} alt=""  className='w-40 -mt-8 drop-shadow-2xl'/>
+          src={item.imageURL} alt=""  className='-mt-8 w-40 h-full object-contain drop-shadow-2xl'/>
         <motion.div
         whileTap={{scale:0.75}}
         className='w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md'>
           <MdShoppingBasket size={18} className='text-white'/>
         </motion.div>
         </div>
-        <div className="w-full flex flex-col justify-end items-end">
-        <p className='text-textColor font-semibold text-base md:text-lg '>Chocolate & Vanilla</p>
-        <p className=" mt-1 text-sm text-textColor">45 Calories</p>
+        <div className="w-full flex flex-col justify-end items-end -mt-8">
+        <p className='text-textColor font-semibold text-base md:text-lg '>{item.title}</p>
+        <p className=" mt-1 text-sm text-textColor">{item.calories}</p>
         <p className='text-lg text-headingColor font-semibold text-center'>
-          <span className='text-sm text-red-500'>$</span>3.45
+          <span className='text-sm text-red-500'>$</span>{item.price}
         </p>
         </div>
       </div>
