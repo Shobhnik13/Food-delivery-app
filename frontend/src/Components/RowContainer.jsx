997ba88b627d7ed26b8ffc74ea7899582580f24d
Shobhnik13 <shobhnikw@gmail.com>
2023-03-02 14:52:39 +0530

    changes

--- frontend/src/Components/RowContainer.jsx
+++ frontend/src/Components/RowContainer.jsx
@@ -19,8 +19,7 @@ const RowContainer = ({flag,data,scrollValue}) => {
       }
       `}
       >
-        {
-          data && data.length>=1 ? data.map(item=>(
+        {data && data.length>=1 ? data.map(item=>(
             <div key={item.id} className='w-300 min-w-[300px] md:min-w-[340px] md:w-340 hover:drop-shadow-lg shadow-md backdrop-blur-lg h-[200px] my-6 bg-cardOverlay rounded-lg p-2'> 
         <div className='w-full flex items-center justify-between'>
           <motion.img
