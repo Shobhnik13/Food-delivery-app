651372d4664a6153f4737313f48886d7e56e2a76
Shobhnik13 <shobhnikw@gmail.com>
2023-03-05 23:53:17 +0530

    started mapping cartItems

--- frontend/src/Components/CartContainer.jsx
+++ frontend/src/Components/CartContainer.jsx
@@ -8,7 +8,7 @@ import {RiRefreshFill} from 'react-icons/ri'
 import { useStateValue } from '../Context/StateProvider'
 import { actionType } from '../Context/Reducer'
 const CartContainer = () => {
-    const [{cartShow},dispatch]=useStateValue()
+    const [{cartShow,cartItems},dispatch]=useStateValue()
     const showCart=()=>{
         dispatch({
             type:actionType.SET_CART_SHOW,
@@ -35,24 +35,29 @@ const CartContainer = () => {
         {/* bottom section div  */}
          <div className='w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col'>
             {/* outer cart item div  */}
-            <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
+            {cartItems && cartItems.map((item)=>{
+                return(
+                    <div key={item.id} className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
                 {/* cart item div */}
                     <div className="w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2">
                     {/* div1 ->image  */}
-                    <img src={dummyImage} alt="" className='w-20 h-20 object-contain rounded-full max-w-[60px]' />
+                    <img src={item.imageURL} alt="" className='w-20 h-20 object-contain rounded-full max-w-[60px]' />
                         {/* div2->name  */}
                         <div className='flex flex-col gap-2'>
-                            <p className='text-base text-gray-50'>Fried Rice</p>
-                            <p className='text-sm block text-gray-300 font-semibold'>$12</p>
+                            <p className='text-base text-gray-50'>{item.title}</p>
+                            <p className='text-sm block text-gray-300 font-semibold'>{item?.price}</p>
                         </div>
                         {/* div3->button  */}
                         <div className='group flex items-center gap-2 ml-auto cursor-pointer'>
                         <motion.div whileTap={{scale:0.75}}><BiMinus className='text-gray-50'/></motion.div>
-                        <p className='w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center'>1</p>
+                        <p className='w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center'>{item.qty}</p>
                         <motion.div whileTap={{scale:0.75}}><BiPlus className='text-gray-50'/></motion.div>
                         </div>
                 </div>
             </div>
+            
+                )
+            })}
             {/* total section  */}
             <div className=" w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
                 {/* outer subtotal section  */}
